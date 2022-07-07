38306c6b3910b268a8d395068c82fd9090934d32
Kisalay <kisalaykisu@gmail.com>
2022-07-07 19:37:51 +0530

    UC1 has been added

--- src/main/java/com/bridgelabz/JavaRegex_UserRegistration.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class JavaRegex_UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration");
-    }
-}
